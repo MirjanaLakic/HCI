a5514668ffa0798a78be140878078aac73bccec1
Gambit88 <djordjeilic15@gmail.com>
2017-03-29 23:10:24 +0200

    povezane liste sa tabelom

--- citanjeKnjiga/citanjeKnjiga/MainWindow.xaml.cs
+++ citanjeKnjiga/citanjeKnjiga/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Collections.ObjectModel;
 using Microsoft.Win32;
 
 
@@ -25,6 +26,26 @@ namespace citanjeKnjiga
         public MainWindow()
         {
             InitializeComponent();
+            Library lib = new Library();
+            lib.Load();
+            ObservableCollection<Book> listRecentRead = new ObservableCollection<Book>();
+            ObservableCollection<Book> listRecentImported = new ObservableCollection<Book>();
+            ObservableCollection<Book> listFavorites = new ObservableCollection<Book>();
+            ObservableCollection<Book> listBooks = new ObservableCollection<Book>();
+            foreach(Book b in lib.books){
+                listBooks.Add(b);
+                if(b.RecentlyAdded)
+                    listRecentImported.Add(b);
+                if (b.Recently)
+                    listRecentRead.Add(b);
+                if(b.Favorite)
+                    listFavorites.Add(b);
+            }
+            this.DG1.ItemsSource = listRecentRead;
+            this.DG2.ItemsSource = listRecentImported;
+            this.DG3.ItemsSource = listFavorites;
+            this.DG4.ItemsSource = listBooks;
+
             
         }
 
