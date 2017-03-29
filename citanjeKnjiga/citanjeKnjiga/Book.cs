76c045c84eb2e2cc7c7d4896aed422d9efeb81f1
Jones312 <djurdjevicnikola03@gmail.com>
2017-03-29 17:02:15 +0200

    Dodate klase Library i Book

---
+++ citanjeKnjiga/citanjeKnjiga/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace citanjeKnjiga
+{
+    class Book
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private bool favorite;
+
+        public bool Favorite
+        {
+            get { return favorite; }
+            set { favorite = value; }
+        }
+
+        private bool recently;
+
+        public bool Recently
+        {
+            get { return recently; }
+            set { recently = value; }
+        }
+
+        private bool recentlyAdded;
+
+        public bool RecentlyAdded
+        {
+            get { return recentlyAdded; }
+            set { recentlyAdded = value; }
+        }
+        
+
+        public Book(string name, bool favorite, bool recently, bool recentlyAdded)
+        {
+            this.Name = name;
+            this.Favorite = favorite;
+            this.Recently = recently;
+            this.RecentlyAdded = recentlyAdded;
+        }
+    }
+}
---
+++ citanjeKnjiga/citanjeKnjiga/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace citanjeKnjiga
+{
+    class Library
+    {
+        List<Book> books;
+        List<Book> recentlyRead;
+
+        public Library()
+        {
+            this.Load();
+        }
+        public void Load()
+        {
+            this.books = new List<Book>();
+            this.recentlyRead = new List<Book>();
+            StreamReader sr = new StreamReader("books.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                bool fav = false;
+                bool re = false;
+                bool ra = false;
+                string[] parts = line.Split(',');
+                string name = parts[0];
+                if (parts[1].Equals("1"))
+                {
+                    fav = true;
+                }
+                if (parts[2].Equals("1"))
+                {
+                    re = true;
+                }
+                if (parts[3].Equals("1"))
+                {
+                    ra = true;
+                }
+                Book book = new Book(name,fav,re,ra);
+                this.books.Add(book);
+                if (book.Recently)
+                {
+                    this.recentlyRead.Add(book);
+                }
+            }
+        }
+    }
+}
